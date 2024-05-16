efd792132b8f4d8c2fa9f3cb871113008c7222c4
Artem <118640498+sesheii@users.noreply.github.com>
2024-05-16 19:31:02 +0300

    add routing

--- react-pool/src/App.js
+++ react-pool/src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'; // якщо ви використовуєте CSS
-import NavBar from './copmonents/NavBar.jsx';
-
-import LoginForm from "./copmonents/LoginForm.jsx"; 
-
+import LoginForm from "./components/LoginForm.jsx"; 
+import HomePage from "./components/HomePage.jsx"; 
+import AboutPage from "./components/AboutPage.jsx"; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <LoginForm />
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div className="App-content">
+          <Routes>
+            <Route path='/' exact element={<HomePage/>} />
+            <Route path='/login' element={<LoginForm/>} />
+            <Route path='/about' element={<AboutPage/>} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
