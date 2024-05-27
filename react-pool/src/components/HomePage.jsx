2bce6cf41294d290ea652adb217cff27fafd5325
FalconAI <61083677+bogdied@users.noreply.github.com>
2024-05-27 13:13:43 +0300

    finished home page

--- react-pool/src/components/HomePage.jsx
+++ react-pool/src/components/HomePage.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import GlobalHeader from './GlobalHeader';
 import GlobalFooter from './GlobalFooter';
-import './HomePage.css'
+import './HomePage.css';
 import { useNavigate } from 'react-router-dom';
+import userIcon from './img/Untitled.png';
 
 const HomePage = () => {
+  const [checkedInUsersCount, setCheckedInUsersCount] = useState(0);
+  const token = localStorage.getItem('access');
+
+  useEffect(() => {
+    const fetchCheckedInUsersCount = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/checked-in-count/', {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        if (response.status === 200) {
+          setCheckedInUsersCount(response.data.checked_in_users_count);
+        }
+      } catch (error) {
+        console.error('Error fetching checked in users count:', error);
+      }
+    };
+
+    fetchCheckedInUsersCount();
+  }, []);
 
   const navigate = useNavigate();
 
@@ -29,14 +52,21 @@ const HomePage = () => {
       <GlobalHeader />
       <div className='HomePageContainer3'>
         <div className='ContentContainer3'>
-          <button className='transparent-button' onClick={handleUserListRedirect}>User List</button>
-          <button className='transparent-button' onClick={handleCreatePoolUserRedirect}>Create User</button>
-          <button className='transparent-button' onClick={handleCreateSubscriptionTypeRedirect}>Subscription Type</button>
+          <div className='Card'>
+            <img src={userIcon} alt='User icon' className='UserIcon' />
+            <div className='CardContent'>
+              <h1 className='Black-Text'>Checked In Users: {checkedInUsersCount}</h1>
+            </div>
+          </div>
+          <button className='transparent-button111' onClick={handleUserListRedirect}>User List</button>
+          <button className='transparent-button111' onClick={handleCreatePoolUserRedirect}>Create User</button>
+          <button className='transparent-button111' onClick={handleCreateSubscriptionTypeRedirect}>Subscription Type</button>
         </div>
       </div>
       <GlobalFooter />
     </div>
   );
+
 }
  
 export default HomePage;
